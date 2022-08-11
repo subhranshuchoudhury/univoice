a9e97b00b40d8969e1dba2c73c5215341d40a4bc
subhranshuchoudhury <subhransuchoudhury00@gmail.com>
2022-08-11 20:45:38 +0530

    fix some

--- src/hooks/useLoadingWithRefresh.js
+++ src/hooks/useLoadingWithRefresh.js
@@ -9,7 +9,7 @@ export function useLoadingWithRefresh() {
         (async () => {
             try {
                 const { data } = await axios.get(
-                    `${process.env.REACT_APP_API_URL}/api/refresh`,
+                    `https://univoicedb.herokuapp.com/api/refresh`,
                     {
                         withCredentials: true,
                     }
